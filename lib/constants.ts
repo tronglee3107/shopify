3609f83e2bdca3ad8e78c58869c70cc094aa5742
Le Dao Duy Trong <trong.le@getvymo.com>
2025-04-15 13:04:17 +0700

    adding head and footer

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Shopify'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Buying the whole world.'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'An Ecommerce Website built with Nextjs and MongoDB'
