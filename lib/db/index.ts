729e1d46b43037da41b1d22c56203ccf04676d67
Le Dao Duy Trong <trong.le@getvymo.com>
2025-04-15 18:19:25 +0700

    adding product banner and connect database

---
+++ lib/db/index.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const cached = (global as any).mongoose || {conn : null, promise : null}
+
+export const connectToDatabase = async (
+    MONGODB_URI = process.env.MONGODB_URI
+) => {
+    if (cached.conn) return cached.conn;
+
+    if (!MONGODB_URI) throw new Error('MONGOBB_URI is missing');
+    cached.promise = cached.promise || mongoose.connect(MONGODB_URI)
+    cached.conn = await cached.promise
+    return cached.conn;
+}
+
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formatNumberWithDecimal = (num: number) : string => {
+  const [int, decimal] = num.toString().split('.')
+  return decimal ? `${int}.${decimal.padEnd(2, '0')}` : int;
+}
+
+export const toSlug = (text: string): string =>
+  text
+    .toLowerCase()
+    .replace(/[^\w\s-]+/g, '')
+    .replace(/\s+/g, '-')
+    .replace(/^-+|-+$/g, '')
